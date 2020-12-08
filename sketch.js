68957c48d3319a3e3ae5633c615e3070565be246
YunSoyoung3 <hanlansong@naver.com>
2020-12-09 05:24:12 +0900

    이전 과정 복사

--- sketch.js
+++ sketch.js
@@ -1,7 +1,18 @@
+let message = ["안녕", "승이야", "생일 축하해", "코로나때문에", "온라인으로 편지를 전해"];
+let index = 0;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
   background(220);
+
+  fill(255);
+  textSize(50);
+  text(message[index], 10, height/2);
+}
+
+function mouseClicked(){
+  index = index +1 ;
 }
